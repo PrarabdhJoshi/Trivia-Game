2dd71b8895c6d8bf11625f91b77d2fac6ee77226
PrarabdhJoshi <prarabdh@ufl.edu>
2018-02-07 17:13:10 -0500

    Federated Login - Fixes #13

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'posts/index' => 'posts#index'
 
   get 'auth/developer', :as => 'developer_auth'
+  get 'auth/github', :as => 'github_auth'
   match 'auth/:provider/callback' => "session#create", :via =>[:get,:post]
 
   get 'session/destroy', :as => 'logout'
